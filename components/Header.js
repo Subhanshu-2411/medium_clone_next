c0047e0c85646eeb958908a49815dcd08f425dc8
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-01-26 15:21:52 +0530

    Initialised Components

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+
+const styles = {}
+const Header = () => {
+
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className={styles.logoContainer}>
+                    <h1>Hello</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
